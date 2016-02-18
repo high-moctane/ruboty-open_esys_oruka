efdbcc9a40fe50bd35a12c3ff1631ccbc08775b4
high-moctane <high.moctane@gmail.com>
2016-02-18 21:15:31 +0900

    Bluetoothの機能を入れた

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ruboty-open_esys_oruka.gemspec
 gemspec
-gem "open_esys-oruka", git: "git@github.com:high-moctane/open_esys-oruka.git"
--- lib/ruboty/open_esys_oruka/actions/oruka.rb
+++ lib/ruboty/open_esys_oruka/actions/oruka.rb
@@ -1,10 +1,12 @@
-require "open_esys-oruka"
+require "parallel"
 require "yaml"
 
 module Ruboty
   module OpenEsysOruka
     module Actions
       class Oruka < Ruboty::Actions::Base
+        MAX_CONNECTION = 6
+
         def call
           message.reply(oruka)
         rescue => e
@@ -12,8 +14,9 @@ module Ruboty
         end
 
         private
+
         def oruka
-          if (result = scan).value?(true)
+          if (result = scan(conf)).value?(true)
             header + "\n" +
               result.keep_if { |k, v| v }.keys.join("\n") +
               "がいます(｀･ω･´)"
@@ -26,12 +29,25 @@ module Ruboty
           YAML.load_file(ENV["OPENESYS_ORUKA_CONF"])
         end
 
-        def scan
-          OpenEsys::Oruka.scan(conf)
-        end
-
         def header
           "#{Time.now.strftime "%R:%S"} 現在、OE室には"
+        end
+
+        def hcitool_name(btaddr)
+          `hcitool name #{btaddr}`
+        end
+
+        def exist?(btaddr)
+          case hcitool_name(btaddr)
+          when "" then false
+          else true
+          end
+        end
+
+        def scan(list)
+          Parallel.map(list, in_threads: MAX_CONNECTION) { |k, v|
+            [k, exist?(v)]
+          }.to_h
         end
       end
     end
