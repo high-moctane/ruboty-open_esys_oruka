ff9660be88f3f0d64762d818ee27fc8b54225884
high-moctane <sakaya_ppkds_yety-highoctane10492@yahoo.co.jp>
2016-04-26 00:54:15 +0900

    動いたのでバージョンをあげる

--- lib/ruboty/open_esys_oruka.rb
+++ lib/ruboty/open_esys_oruka.rb
@@ -4,12 +4,5 @@ require "ruboty/handlers/open_esys_oruka"
 
 module Ruboty
   module OpenEsysOruka
-    class << self
-      NAMESPACE = :open_esys_oruka
-
-      def table
-        robot.brain.data[NAMESPACE] ||= {}
-      end
-    end
   end
 end
--- lib/ruboty/open_esys_oruka/version.rb
+++ lib/ruboty/open_esys_oruka/version.rb
@@ -1,5 +1,5 @@
 module Ruboty
   module OpenEsysOruka
-    VERSION = "0.1.1.3"
+    VERSION = "0.1.2"
   end
 end
